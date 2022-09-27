17e153f9c3e05e53b8612add6cc3891484fa68f0
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2022-09-27 10:23:23 +0500

    arrow functions added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0,
+        }
+    }
+
+    addOne = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    deleteOne = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    reset = () => {
+        this.setState({
+            count: this.state.count = 0
+        })
+    }
   render() {
     return (
       <div className='container'>
         <h1>Counter</h1>
         <div className='circle--container'>
-            <p>0</p>
+            <p>{this.state.count}</p>
         </div>
         <div className='buttons--container'>
-            <button className='plus'>+</button>
-            <button className='reset'>&#x21bb;</button>
-            <button className='minus'>-</button>
+            <button className='plus' onClick={this.addOne}>+</button>
+            <button className='reset' onClick={this.reset}>&#x21bb;</button>
+            <button className='minus' onClick={this.deleteOne}>-</button>
         </div>
       </div>
     )
