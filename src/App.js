ddc1c447587163060fd131296e79634cb053fcee
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2022-09-27 10:11:57 +0500

    modified css and js files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default class App extends Component {
   render() {
     return (
       <div className='container'>
+        <h1>Counter</h1>
+        <div className='circle--container'>
+            <p>0</p>
+        </div>
+        <div className='buttons--container'>
+            <button className='plus'>+</button>
+            <button className='reset'>&#x21bb;</button>
+            <button className='minus'>-</button>
+        </div>
       </div>
     )
   }
